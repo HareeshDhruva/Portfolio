28aea4719bc8ea72a635ee3fd0bc3704fb237805
Hareesh Dhruva <88025959+HareeshDhruva@users.noreply.github.com>
2023-10-18 18:38:05 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/HareeshDhruva",
   resume:"https://drive.google.com/file/d/1l0_sdhjSaPG1ntXwoTaDLNUNWDQ9dXL0/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/hareesh-dhruva-13b151250/",
+  linkedin: "https://www.linkedin.com/in/hareesh-dhruva-797240296",
   twitter: "https://twitter.com/dhruva_hareesh",
   insta: "https://www.instagram.com/hareesh_dhruva/",
   facebook: "https://www.facebook.com/hareeshdhruva",
